c0a38cfa250b656c6c028958247fa9b19f0704e4
Sahariar Kabir <sronok@gmail.com>
2023-12-02 17:24:14 +0600

    initial setup done

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Http\Controllers\CollectionsController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+})->middleware(['verify.shopify'])->name('home');
+
+
+Route::get('/shop', [CollectionsController::class , 'group.index'])->middleware(['verify.shopify'])->name('group.index');
+
